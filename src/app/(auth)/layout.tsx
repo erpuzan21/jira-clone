b43336263e9a74d3767655e8ddd85b5ba1c0d162
Pujan Shrestha <erpuzan21@gmail.com>
2026-09-18 00:21:12 +0545

    remembered (auth) layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <main className="bg-neutral-100 min-h-screen">
+    <main className="bg-red-200 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
 
         <nav className="flex justify-between items-center">
