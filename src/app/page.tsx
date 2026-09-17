fd6c7f1735a751d5a0d111f3f529d36ddc351c7e
Pujan Shrestha <erpuzan21@gmail.com>
2026-09-17 20:10:57 +0545

    Updated sign in and home pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,34 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex gap-4 p-4">
-      <input /> 
-      <Button>Primary</Button>
+    <div>
+      <div className="flex gap-4 p-4 bg-amber-200">
+        <input />
+        <Button>Primary</Button>
 
-      <Button variant="default">
-        Delete
-      </Button>
+        <Button variant="outline">
+          Delete
+        </Button>
 
-      <Button className="bg-green-500 hover:bg-green-600">
-        Save
-      </Button>
+        <Button className="bg-green-500 hover:bg-green-600">
+          Save
+        </Button>
 
-      <Button size="lg">
-        Large Button
-      </Button>
-      
+        <Button size="lg">
+          Large Button
+        </Button>
+
+        <Button className="bg-red-500 hover:bg-green-600">
+          exit
+        </Button>
+
+      </div>
+
+      <div className="flex gap-4 p-5 bg-black">
+        <Button variant="secondary">
+          PUZAN
+        </Button>
+      </div>
     </div>
-    
-    
   );
 }
