54fa9c4446d9cd002881dd96240d96e50ce16a84
Pujan Shrestha <erpuzan21@gmail.com>
2026-09-18 21:57:24 +0545

    edited SignInCard and added Login buttons

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,4 +5,4 @@ const SignInPage = () => {
   return <SignInCard/>;
 };
 
-export default SignInPage;
+ export default SignInPage;
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,21 +6,25 @@ import {
   CardTitle
 } from "@/components/ui/card";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import{Separator} from "@/components/ui/separator";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Input } from "@/components/ui/input";
-
-export const SignInCard = () => {
+import { Button } from "@/components/ui/button";
+  export const SignInCard = () => {
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
 
-      <CardHeader className="flex items-center justify-center text-center">
+      <CardHeader className="flex justify-center p-4 items-center text-center">
         <CardTitle className="text-2xl">
           Welcome back!
         </CardTitle>
       </CardHeader>
 
       <div className="px-7 mb-2">
-        <DottedSeparator />
+        < DottedSeparator/>
       </div>
 
       <CardContent className="p-7">
@@ -35,9 +39,39 @@ export const SignInCard = () => {
             disabled={false}
           />
 
+          <Input
+            required
+            type="password"
+            value=""
+            onChange={() => {}}
+            placeholder="Enter password here"
+            disabled={false}
+            min={5}
+            max={20}
+          />
+          <Button disabled={false} type="submit"  p-2 className="w-full">
+            Login
+          </Button>
+
         </form>
       </CardContent>
+          <div className="">
+            <Separator />
+          </div>
+          <CardContent >
+            <Button disabled={false} type="submit" className="w-full" variant="secondary" >
+              <FcGoogle className="mr-2" size={20} /> 
+              Login with Google
+            </Button>
+          </CardContent>
 
+            <CardContent >
+            <Button disabled={false} type="submit" className="w-full" variant="secondary" >
+              <FaGithub className="mr-2" size={20} /> 
+              Login with Github
+            </Button>
+          </CardContent>
+  
     </Card>
   );
 };
