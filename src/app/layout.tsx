278f960c2a106ac3bbd1fdfd76e9c7653da0bd4d
Pujan Shrestha <erpuzan21@gmail.com>
2026-09-18 12:35:18 +0545

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "antialiased min-h-screen")}>
+      <body className={cn(poppins.className, "antialiased min-h-screen")}>
         {children}
       </body>
     </html>
